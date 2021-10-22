29fff0815fce2e70fa3ae62c918d9ff671ac6ff8
jkobyl4 <55068427+Jan3312@users.noreply.github.com>
2021-10-22 00:41:15 -0500

    HTML/CSS/JS Main sign-in Page

--- myapp/app.js
+++ myapp/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var loginRouter = require('./routes/login');
 
 var app = express();
 
@@ -19,8 +20,19 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+//app.use('/', indexRouter);
+//app.use('/users', usersRouter);
+//app.use('/login', indexRouter);
+
+// Routes
+app.get('', (req, res) => {
+  res.render('index', { title: 'Home Page'})
+})
+
+app.get('/login', (req, res) => {
+  res.render('login', { title: 'login'})
+})
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- myapp/routes/login.js
+++ myapp/routes/login.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+
+/* GET create account page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.setHeader("Content-Type", "text/html")
+  res.render('login', { title: 'login' });
 });
 
 module.exports = router;
-
-
 
