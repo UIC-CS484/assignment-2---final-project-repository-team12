b3d0ff97424caea3ec839c09bb908c26b3b4d914
jkobyl4 <55068427+Jan3312@users.noreply.github.com>
2021-10-22 20:02:03 -0500

    Final Changes(for now)

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
-"# assignment-2---final-project-repository-team12" 
-# assignment-2---final-project-repository-team12
+Nasa Image Finder
+
+CS 484 final project.
+
+## Table of Contents
+
+- **[Build and Run](#build-and-run)**<br>
+- **[Team Members](#team-members)**<br>
+- **[Development Tools](#development-tools)**<br>
+
+## Build and Run
+
+From the root directory, run:
+
+```
+$ npm start
+```
+
+Once the server is running, you can navigate to [http://localhost:3000/](http://localhost:3000/) to use the application.
+
+
+
+## Team Members
+
+### Jan Kobylarz
+
+- [jkobyl4@uic.edu](jkobyl4@uic.edu)
+- [https://github.com/Jan3312](https://github.com/Jan3312)
+
+
+## Development Tools
+
+1. [Visual Studio Code](https://code.visualstudio.com/) to develop the project
+2. [Git Bash](https://git-scm.com/) to launch the project
+3. [Chrome](https://www.google.com/chrome/) to test the project
+4. [GitHub](https://github.com/) to host the project
+
+
+## Development Languages/frameworks
+Front-end: JS, CSS, PUG, Bootstrap
+Back-end: Node.js, Express, JS
+Testing-tools: jest
+Authentication: Passport.js
+
+
+
--- app.js
+++ app.js
@@ -1,17 +1,47 @@
+if(process.env.NODE_ENV != 'production')
+{
+  require('dotenv').config()
+}
+
+//Libraries
 var createError = require('http-errors');
-var express = require('express');
+const express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const bcrypt = require('bcrypt');
+const passport = require('passport');
+const flash = require('express-flash')
+const session = require('express-session')
+const methodOverride = require('method-override')
 
+const initializePassport = require('./passport-config');
+initializePassport( passport, email => users.find(user => user.email == email),
+                    id => users.find(user => user.id == id) )
+
+//Store user data, replace with a database for next homework
+const users = [];
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var loginRouter = require('./routes/register');
+const { resolveTxt } = require('dns');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
+app.use(express.urlencoded({ extended: false}))
+app.use(flash())
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(methodOverride('_method'))
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,8 +49,79 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+//app.use('/', indexRouter);
+//app.use('/users', usersRouter);
+//app.use('/login', indexRouter);
+
+// Routes
+app.get('/main', checkAuthenticated, (req, res) => {
+  res.render('main', { title: 'main page'})
+})
+
+app.post('/main', checkNotAuthenticated, (req, res) => {
+    res.redirect('/')
+})
+
+app.get('/', checkNotAuthenticated, (req, res) => {
+  res.render('index', { title: 'login page'})
+})
+
+app.post('/', checkNotAuthenticated, passport.authenticate('local', {
+  successRedirect: '/main',
+  failureRedirect: '/',
+  failureFlash: true
+}))
+
+
+app.get('/register', checkNotAuthenticated, (req, res) => {
+  res.render('register', { title: 'register page'})
+})
+
+app.post('/register', checkNotAuthenticated, async (req, res) => {
+  try{
+    const hashedPassword = await bcrypt.hash(req.body.password, 10)
+    users.push({
+      id: Date.now().toString(),
+      email: req.body.email,
+      password: hashedPassword,
+      terms: req.body.checkbox
+    })
+ 
+
+
+    res.redirect('/')
+  } catch {
+      if(terms != 'on'){res.redirect('/register')}
+    res.redirect('/register')
+  }
+
+  console.log(users)
+})
+
+
+app.delete('/logout', (req, res) => {
+  req.logOut()
+  res.redirect('/')
+})
+
+function checkAuthenticated(req, res, next)
+{
+  if(req.isAuthenticated())
+  {
+    return next()
+  }
+  res.redirect('/')
+}
+
+function checkNotAuthenticated(req, res, next)
+{
+  if(req.isAuthenticated())
+  {
+    return res.redirect('/main')
+  }
+  next()
+}
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
